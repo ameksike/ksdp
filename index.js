349f92f374aff95dd4bdebdf6431836f7c0d6905
amembrides <antonio.membrides@sita.aero>
2023-04-15 14:10:41 +0200

    add di support

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const Strategy = require('./src/behavioral/Strategy');
 const Observer = require('./src/behavioral/Observer');
 const Factory = require('./src/creational/Factory');
 const IoC = require('./src/integration/IoC/IoC');
+const Dip = require('./src/integration/Dip');
 const KsProxy = require('./src/structural/Proxy');
 
 module.exports = {
     creational: { Factory },
     behavioral: { Strategy, Observer },
-    integration: { IoC },
+    integration: { IoC, Dip },
     structural: { Proxy: KsProxy }
 }
