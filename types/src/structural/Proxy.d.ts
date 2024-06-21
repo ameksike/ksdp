c3399e513d9f911553a147136d9af1398965eef3
Antonio Membrides Espinosa <tonykssa@gmail.com>
2024-06-21 17:05:31 +0200

    fix proxy type

--- types/src/structural/Proxy.d.ts
+++ types/src/structural/Proxy.d.ts
@@ -1,19 +1,19 @@
 export = KsProxy;
 /**
  * @description Extendable Proxy class implementation
- * @author		Antonio Membrides Espinosa
- * @email		tonykssa@gmail.com
- * @date		11/05/2020
- * @copyright  	Copyright (c) 2020-2050
- * @license    	GPL
- * @version    	1.0
+ * @author      Antonio Membrides Espinosa
+ * @email       tonykssa@gmail.com
+ * @date        11/05/2020
+ * @copyright   Copyright (c) 2020-2050
+ * @license     GPL
+ * @version     1.0
  **/
 declare class KsProxy {
     /**
      * @description Proxy builder
-     * @returns {Object}
+     * @returns {KsProxy}
      */
-    build(): any;
+    build(): KsProxy;
     /**
      * @description Must be overridden in the child class to define how methods or properties of the controlled class are read
      * @virtual
@@ -26,11 +26,11 @@ declare class KsProxy {
     /**
      * @description Must be overridden in the child class to define how properties of the controlled class are set
      * @virtual
-     * @param {Object} target
-     * @param {String} key
+     * @param {any} target
+     * @param {string | number | symbol} key
      * @param {*} value
      * @param {Object} receiver
      */
-    set(target: any, key: string, value: any, receiver: any): any;
+    set(target: any, key: string | number | symbol, value: any, receiver: any): any;
     #private;
 }
